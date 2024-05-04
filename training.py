63f4937b966dc3d36f6f304b7224d057220b021a
thejewels00 <jchennak42@gmail.com>
2024-05-04 03:39:25 +0100

    training data is ready

--- training.py
+++ training.py
@@ -28,3 +28,34 @@ for intent in intents['intents']:
 
 print(documents)
 
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+
+
+    random.shuffle(training)
+    training = np.array(training)
+
+    training_x = list(training[:, 0])
+    training_y = list(training[:, 1])
